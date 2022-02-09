f4984c74d0c716934fd74d9ef63e93d92746f4ca
laysarochamartins <69978713+laysaro@users.noreply.github.com>
2022-02-09 22:44:14 +0100

    Add files via upload

---
+++ let idade = 18.js
@@ -0,0 +1,20 @@
+let idade = 18
+if (idade>=18){
+    console.log("Cadastro permitido")
+}else {
+    console.log ("Menor de 18 anos, cadastro não permitido")
+} let data = "28/12/2021", dataAtual = "25/12/2021"
+
+if(data >= dataAtual) {
+    console.log(" É possivel realizar o cadastro") 
+    }else {
+    console.log ("Consulte a data do envento e tente novamente !!")
+} 
+let quantidade = 100
+if(quantidade <=100) {
+    console.log ("Cadastro realizado")
+}else {
+    console.log ("Excedeu a quantidade de vagas disponiveis")
+}
+const participantes = ["Patrick,Maria,João,Pedro,José"]
+console.log (participantes)
